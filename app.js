3b7626291cec91b5525c3ac32ac928fbfd810cc1
”LevchenkoLudmila” <milayahuliganka737@gmail.com>
2022-09-08 19:03:41 +0300

    seive chenges

--- app.js
+++ app.js
@@ -1,29 +1,71 @@
 const express = require('express');
-const multer = require('multer')
-const path = require('path');
+const multer = require('multer');
+//const localMulter = multer({dest:'uploads'}).single('filedata');
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) =>{
+//       cb(null, "uploads");
+//   },
+//   filename: (req, file, cb) =>{
+//       cb(null, file.originalname);
+//   }
+// });
+//const localMulter = multer({storage}).single('filedata');
+const localMulter = multer({path:'uploads'}).single('filedata1');
+//const path = require('path');
 const crypto = require('crypto');
 const fs = require('fs');
 const objHash = require ('object-hash') ;
+const Stream = require('stream');
 
 const server = express();
-
- server.listen(3000);
+const upload = multer();
  server.set('view engine', 'ejs');
  server.set('views','./views');
  
-server.use(express.static('public'));
-server.use(multer({dest:'uploads'}).single('filedata'));
+server.use(express.static('./public'));
 
-// server.get('/form', (req, res) => {
-//   res.render('form');
+ //работа с формой
+
+//  server.post('/uploads', function (req, res, next) {
+
+//   let filedata = req.file;
+//   console.log(filedata);    
 // });
 
-// server.get('/uploads/:name', (req,res) => {
-//   res.send('req.file');
-//  })
- server.get('/',(req,res) =>{
-  res.render('index');
- });
+server.post('/upload1',localMulter,(req, res, next)=> {
+  console.log(req);
+
+  // const getFile = async (req, res) => {
+
+  //    const readStream = await fs.createReadStream(req.file.path);
+
+  // }; 
+  //  const fileBuffer = fs.readFile(req.file.path);
+
+  // const hashSum = crypto.createHash('sha256');
+  // hashSum.update(getFile);
+  // const hex = hashSum.digest('hex');
+
+  // stream.pipe(readStream);
+  // stream.pipe(hex);
+
+  // console.log('Nazva', hex);
+  // fs.rename(req.file.path, `./uploads/${hex}`, (err) => {
+  //     if (err) throw err;
+  //     console.log('renamed complete');
+  //   });    
+});
+
+server.get('/',(req,res) =>{
+  res.render('form');
+});
+  
+  // server.get('/form', (req, res) => {
+  //   res.render('form');
+  // });
+  // server.get('/uploads/:name', (req, res) => {
+  //     res.send(req.file);
+  // }); 
  //отдать страницы
 // server.get('/pages/contacts',(req,res) =>{
 //     const contactsUs = [
@@ -47,10 +89,9 @@ server.use(multer({dest:'uploads'}).single('filedata'));
 //    res.redirect('contacts');
 //  });
 
- //кфбота с формой
-
- //404 if mistake
+// 404 if mistake
 // server.use((req,res) =>{
 //    res.statusCode = 404;
 //    res.render('error');
 //  });
+ server.listen(3000);
